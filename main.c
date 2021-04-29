3da18f4711d4c34fe9f4b6ac360950a9cea83da3
FolkishAcorn <etwe226@uky.edu>
2021-04-29 05:20:35 -0400

    Updated insert variable function     - Automatically reallocates space if dictionary is full     - removed MAX_NUM_VARIABLES Added descriptions for functions Added function to execute commands     - for use with ! Updated cd     - added error Added signal handler to catch ^C

--- main.c
+++ main.c
@@ -10,8 +10,9 @@
 void dieWithError(char *error); 
 
 #define MAX_INPUT_SIZE 256
-#define MAX_NUM_VARIABLES 10
 #define TOKEN_STRUCT_SIZE 260
+
+int available_variable_space = 10;
 
 typedef struct variable{
 
@@ -102,6 +103,22 @@ void inputParser(token scanned_input){
 }
 
 /**
+ * @brief For use after parent has forked, 
+ *        Changes working directory to path and runs 
+ *        command passes args/envp
+ * 
+ * @param path 
+ * @param command 
+ * @param args 
+ * @param envp  
+ */
+void execute( char *path, char *command, char *args[], char envp*[]) {
+    cd(path);
+    int status = execve(command, args, envp);
+    if (status == -1) dieWithError("Error: Could not execute program\n");
+}
+
+/**
  * @brief Wrapper for establising a handler;
  *        Function from lecture slides (3/8)
  * 
@@ -122,51 +139,94 @@ sighandler_t setSignalHandler(int signum, sighandler_t handler){
     return old_action.sa_handler;
 }
 
+/**
+ * @brief If name already exists in dictionary, value is updated; otherwise, inserts new variable into dictionary
+ * 
+ * @param dictionary 
+ * @param name 
+ * @param value 
+ * @return int Returns 0 on successful update/insert
+ */
 int insertVariable(variable *dictionary, char *name, char *value){
     // if name already exists, value is updated
-    for (int i = 0; i < MAX_NUM_VARIABLES; i++){
+    for (int i = 0; i < available_variable_space; i++){
         if (dictionary[i].name == name){
             dictionary[i].value = value;
             return 0;
         }
     }
     // insert new name/variable into dictionary
-    for (int i = 0; i < MAX_NUM_VARIABLES; i++){
+    for (int i = 0; i < available_variable_space; i++){
         if (dictionary[i].name == NULL){
             dictionary[i].name = name;
             dictionary[i].value = value;
             return 0;
         }
     }
-    else // not enough space in dictionary
-        return -1;
+    
+    // use realloc to allocate more space if dictionary is full 
+    available_variable_space += 1;
+    dictionary = (variable*)realloc(dictionary, available_variable_space * sizeof(variable));
+    dictionary[available_variable_space - 1].name = name;
+    dictionary[available_variable_space - 1].value = value;
+    return 0;
 }
 
+/**
+ * @brief Searches for variable in dictionary 
+ * 
+ * @param dictionary 
+ * @param name 
+ * @return Returns the value of the stored variable name from dictionary, 
+ *         returns NULL if not found
+ */
 char* searchVariable(variable *dictionary, char *name){
-    for (int i; i < MAX_NUM_VARIABLES; i++){
+    for (int i; i < available_variable_space; i++){
         if (dictionary[i].name == name)
             return dictionary[i].value;
     }
     return NULL;
 }
 
+/**
+ * @brief Sets dictionary variable to NULL
+ * 
+ * @param dictionary 
+ * @param name 
+ * @return returns 0 if successful, 
+ *         returns -1 if name isn't in dictionary
+ */
 int unsetVariable( variable *dictionary, char *name){
-    for (int i = 0; i < MAX_NUM_VARIABLES; i++){
+    for (int i = 0; i < available_variable_space; i++){
         if (dictionary[i].name == name){
             dictionary[i].name = NULL;
+            dictionary[i].value = NULL;
+            return 0;
         }
     }
+    return -1;
 }
 
+/**
+ * @brief Prints predefined variables and dictionary variables 
+ * 
+ * @param dictionary 
+ * @param PATH 
+ * @param CWD 
+ * @param PS 
+ */
 void printVariables(variable *dictionary, char* PATH, char* CWD, char* PS){
     // prints predefined variables 
+    printf("Predefined Variables\n");
+    printf("--------------------\n");
     printf("PATH: %s\n", PATH);
     printf("CWD: %s\n", CWD);
     printf("PS: %s\n", PS);
 
     // prints user defined variables 
     printf("User Defined Variables\n");
-    for(int i = 0; i < MAX_NUM_VARIABLES; i++){
+    printf("----------------------\n");
+    for(int i = 0; i < available_variable_space; i++){
         if(dictionary[i].name != NULL) 
             printf("%s: %s\n", dictionary[i].name, dictionary[i].value);
     }
@@ -176,18 +236,26 @@ void printVariables(variable *dictionary, char* PATH, char* CWD, char* PS){
  * @brief cd command changes current working directory (cwd) to path
  * 
  * @param path 
- * @return char* updated cwd 
+ * @return char * Updated CWD 
  */
 char* cd(char *path){
-    chdir(path);
+    int status = chdir(path);
+    if (status == -1) 
+        dieWithError("Error: could not change directory\n");
     return getcwd(NULL, PATH_MAX);
 }
 
+void quit(){
+
+    exit(0);
+}
+
 int main(){
-    
+
+    setSignalHandler(SIGINT, SIG_IGN); // catches ^C 
     DIR *directory;
     struct dirent *de;
-    char *PATH = "/bin:/usr/bin";
+    char *PATH = "/usr/bin"; // in testing, /bin:/usr/bin did not exist; using path "/usr/bin" executed programs properly
     char *CWD;
     char *PS = "> ";
     char usr_input[MAX_INPUT_SIZE];
@@ -197,7 +265,7 @@ int main(){
     if (getcwd(CWD, PATH_MAX) == NULL) dieWithError("getcwd error");
 
     variable *dictionary;
-    dictionary = (variable *)calloc(MAX_NUM_VARIABLES, sizeof(variable));
+    dictionary = (variable *)calloc(available_variable_space, sizeof(variable));
 
     for(;;){
         fgets(usr_input, MAX_INPUT_SIZE, stdin);
